01518d153e27478ba86ec341382d227c67ee1a29
dlckdtjq153 <dlckdtjq153@gmail.com>
2022-05-26 10:01:49 +0900

    commit

--- Get report results.py
+++ Get report results.py
@@ -12,25 +12,20 @@ def solution(id_list, report, k):
         report_attacker = i.split(' ');
         if report_attacker[1] == 'muzi':
             m += 1;
-            # if k == m:
             logMsg(report_attacker[0], report_attacker[1])
         elif report_attacker[1] == 'frodo':
             f += 1;
-            # if k == f:
             logMsg(report_attacker[0], report_attacker[1])
         elif report_attacker[1] == 'apeach':
             a += 1;
-            # if k == a:
             logMsg(report_attacker[0], report_attacker[1])
         else:
             n += 1;
-            # if k == n:
             logMsg(report_attacker[0], report_attacker[1])
     answer.append(m);
     answer.append(f);
     answer.append(a);
     answer.append(n);
-    print(answer);
     return answer
 
 solution(id_list, report, k);
