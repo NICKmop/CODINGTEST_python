a8deda6ec799e8aa5dac58b502a5c9e03a83cb93
dlckdtjq153 <dlckdtjq153@gmail.com>
2022-06-02 14:46:54 +0900

    commit

---
+++ NUmberWordAndengWords.py
@@ -0,0 +1,63 @@
+from ntpath import join
+from pymysql import NULL
+
+def checkWithNumber(word):
+    
+    attachWord = '';
+    wordBox = '';
+    NumberBox = 0;
+
+    for i in word:
+        attachWord += i;
+        if attachWord == "zero":
+            wordBox += attachWord;
+            attachWord = "";
+
+        elif attachWord == "one":
+            wordBox += attachWord;
+            attachWord = "";
+        elif attachWord == "two":
+            wordBox += attachWord;
+            attachWord = "";
+        elif attachWord == "three":
+            wordBox += attachWord;
+            attachWord = "";
+        elif attachWord == "four":
+            wordBox += attachWord;
+            attachWord = "";
+        elif attachWord == "five":
+            wordBox += attachWord;
+            attachWord = "";
+        elif attachWord == "six":
+            wordBox += attachWord;
+            attachWord = "";
+        elif attachWord == "seven":
+            wordBox += attachWord;
+            attachWord = "";
+        elif attachWord == "eight":
+            wordBox += attachWord;
+            attachWord = "";
+        elif attachWord == "nine":
+            wordBox += attachWord;
+            attachWord = "";
+        elif attachWord == "4":
+            wordBox += attachWord;
+            attachWord = "";
+            # attachWord = "";
+
+        print(attachWord)
+    print(wordBox);
+    return NULL;
+
+
+def solution(s):
+    checkWithNumber(s);
+    answer = 0
+    return answer
+
+# "one4seveneight"	1478
+# "23four5six7"	234567
+# "2three45sixseven"	234567
+# "123"	123
+
+solution("one4seveneight");
