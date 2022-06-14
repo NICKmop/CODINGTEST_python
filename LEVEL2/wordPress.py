2bd683daa0e310249cfd93ddbffa4f7d26f57902
dlckdtjq153 <dlckdtjq153@gmail.com>
2022-06-14 14:26:48 +0900

    commit

--- LEVEL2/wordPress.py
+++ LEVEL2/wordPress.py
@@ -4,12 +4,14 @@ def solution(s):
     # 1개 단위(step)부터 압축 단위를 늘려가며 확인
     for step in range(1, len(s) // 2 + 1):
         compressed = ""
-        prev = s[0:step]  # 앞에서부터 step만큼의 문자열 추출
+        prev = s[0:step]  # 앞에서부터 step만6큼의 문자열 추출
         count = 1
     
     # 단위(step) 크기만큼 증가시키며 이전 문자열과 비교
+        print("step  : " , step);
+        print("len(s) : " , len(s));
         for j in range(step, len(s), step):
-        
+        # 1~8 2~8 3~8
         # 이전 상태와 동일하다면 압축 횟수(count) 증가
             if prev == s[j:j + step]:
                 count += 1
@@ -27,3 +29,5 @@ def solution(s):
         answer = min(answer, len(compressed))
 
     return answer
+
+solution(s="aabbaccc");
